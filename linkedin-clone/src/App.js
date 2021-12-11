cdf834d7ab938cfa79b373717cef2adad58adc8d
BAATTS <baattts@gmail.com>
2021-12-10 22:57:47 -0800

    Adding sidebar

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <Header />
       {/* App Body */}
-        {/* Sidebar */}
-        {/* Feed */}
-        {/* Widgets */}     
+      <div className="app_body">
+          < Sidebar />
+          {/* Feed */}
+          {/* Widgets */}     
+      </div>
     </div>
   );
 }
---
+++ linkedin-clone/src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar_top">
+                <img src="http://placehold.it/" alt="placeholder"/>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
