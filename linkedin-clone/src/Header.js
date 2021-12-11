9b548ee18a89de9766988b906c3cc177711565a6
BAATTS <baattts@gmail.com>
2021-12-10 22:46:42 -0800

    adding files

---
+++ linkedin-clone/src/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Header.css'
+import HeaderOption from './HeaderOption.js';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
+                alt="Linkedin Logo" />
+
+                <div className="header_search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+
+            <div className="header_right">
+                <HeaderOption Icon={HomeIcon} title='Home'/>
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network'/>
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+                <HeaderOption Icon={ChatIcon} title='Chat'/>
+                <HeaderOption Icon={NotificationIcon} title='Notifications'/>
+                <HeaderOption Icon={AccountCircleIcon} title='Account'/>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
